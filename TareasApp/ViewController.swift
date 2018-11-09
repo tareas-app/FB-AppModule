d21ef2cfbd59c6431d389e155c5e52b6006a1afc
Tareas <apptareas@gmail.com>
2018-11-09 12:50:46 +0100

    Initial Commit

---
+++ TareasApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TareasApp
+//
+//  Created by Trèvier Gittens on 09/11/2018.
+//  Copyright © 2018 Gindora Global. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
